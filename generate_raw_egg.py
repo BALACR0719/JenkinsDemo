0d19faacc8c35c17522bc97a3acc08d3782de477
BALACR0719 <77215025+BALACR0719@users.noreply.github.com>
2021-01-13 13:51:01 +0530

    Delete generate_raw_egg.py

--- generate_raw_egg.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name='RawData',
-      version='0.0.5',
-      description='Ingesting Raw Data',
-      author='DataLake',
-      author_email='HQ-Dist-GTI-DataLake-DG@gap.com',
-      packages=find_packages(exclude=['*enrichment*','*flattening*']),
-      zip_safe=False)
